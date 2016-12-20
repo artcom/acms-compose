297daf2094f3476c00e248198e7de9969bf1bc8e
Guido Lorenz <guido.lorenz@artcom.de>
2016-12-20 15:19:10 +0100

    Use default value for gitJsonApi parameter

--- src/apis/gitJsonApi.js
+++ src/apis/gitJsonApi.js
@@ -1,8 +1,9 @@
 import axios from "axios"
+import { format, parse } from "url"
 import querystring from "querystring"
 
 const params = querystring.parse(window.location.search.substring(1))
-const url = params.gitJsonApi
+const url = params.gitJsonApi || defaultUrl()
 
 export async function loadData(version = "master") {
   const response = await axios.get(version, { baseURL: url })
@@ -14,4 +15,14 @@ export async function loadData(version = "master") {
 
 export async function updateContent(content, version) {
   await axios.post(`${version}/content`, content, { baseURL: url })
+}
+
+function defaultUrl() {
+  const url = parse(window.location.href)
+
+  return format({
+    protocol: url.protocol,
+    slashes: true,
+    host: `git-json-api.${url.host}`
+  })
 }
