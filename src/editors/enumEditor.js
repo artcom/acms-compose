d609d43d44afc73e7c6a193cfe5c56fa8a89536e
Guido Lorenz <guido.lorenz@artcom.de>
2017-02-10 14:25:40 +0100

    Support non-string enum values

--- src/editors/enumEditor.js
+++ src/editors/enumEditor.js
@@ -2,10 +2,20 @@ import React from "react"
 import { FormControl } from "react-bootstrap"
 
 export default function EnumEditor({ field, onChange }) {
+  function onChangeEnum(event) {
+    onChange({
+      target: {
+        value: field.values[event.target.value].value
+      }
+    })
+  }
+
+  const selectedIndex = field.values.findIndex(value => value.value === field.value)
+
   return (
-    <FormControl componentClass="select" value={ field.value } onChange={ onChange }>
-      { field.values.map(value =>
-        <option key={ value.value } value={ value.value }>{ value.name }</option>
+    <FormControl componentClass="select" value={ selectedIndex } onChange={ onChangeEnum }>
+      { field.values.map((value, index) =>
+        <option key={ index } value={ index }>{ value.name }</option>
       ) }
     </FormControl>
   )
